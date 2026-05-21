8b4fb7b729a49bb6bb59902f6167a3fe47bc7cbc
LY <2085580560@qq.com>
2026-05-21 17:56:50 +0800

    init

--- Extension/entrypoints/background.js
+++ Extension/entrypoints/background.js
@@ -1,6 +1,28 @@
 export default defineBackground(() => {
     console.log('Background script started', {id: browser.runtime.id});
-
+    browser.action.onClicked.addListener((tab) => {
+        console.log('扩展图标被点击', tab);
+    });
+    browser.runtime.onMessage.addListener(async (message) => {
+        if (message.type === 'popup_opened') {
+            console.log('popup 已打开');
+            const currentWindow = await browser.windows.getCurrent();
+            const width = Math.max(currentWindow.width - 400, 300);
+            const height = Math.max(currentWindow.height - 300, 200);
+            const left = Math.round((currentWindow.width - width) / 2);
+            const top = Math.round((currentWindow.height - height) / 2);
+            // 打开居中窗口
+            await browser.windows.create({
+                url: "/popup_true.html", // 你的 popup 页面 //这里需要是实际的popup.html，而不是/popup/index.html,不然会找不到
+                type: "popup", // 无边栏窗口
+                // type: "normal", // 无边栏窗口
+                width,
+                height,
+                left,
+                top,
+            });
+        }
+    });
 
 
 })
--- Extension/entrypoints/popup/main.ts
+++ Extension/entrypoints/popup/main.ts
@@ -1,10 +1,12 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import {createPinia} from "pinia";
+// import { createApp } from 'vue';
+// import './style.css';
+// import App from './App.vue';
+// import {createPinia} from "pinia";
+//
+// const app = createApp(App);
+// const pinia = createPinia();
+//
+// app.use(pinia);
+// app.mount('#app');
 
-const app = createApp(App);
-const pinia = createPinia();
-
-app.use(pinia);
-app.mount('#app');
+browser.runtime.sendMessage({ type: 'popup_opened' });
